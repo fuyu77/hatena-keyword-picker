2d9a0f758c86552f0fa92a1946a43a71bc585202
fuyu77 <kiwi.vc@gmail.com>
2022-10-19 23:46:37 +0900

    Refactor

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const puppeteer = require('puppeteer')
   // 記事中のはてなブログタグを配列として取得する
   const keywords = await page.$$eval('a.keyword', (nodes) => nodes.map((node) => node.innerText))
   // 取得したはてなブログタグの重複数を集計する
-  const keywordsWithCount = {}
-  keywords.forEach((keyword) => {
-    keywordsWithCount[keyword] ? keywordsWithCount[keyword]++ : keywordsWithCount[keyword] = 1
-  })
+  const keywordsWithCount = keywords.reduce((result, keyword) => {
+    return {
+      ...result,
+      [keyword]: result[keyword] ? result[keyword] + 1 : 1
+    }
+  }, {})
   console.log(keywordsWithCount)
   await browser.close()
 })()
